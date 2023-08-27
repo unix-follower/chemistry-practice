e97a12563a8f41e9dc1445f42b2aff6938bef3bd
Artsem_Nikitsenka <artsem_nikitsenka@epam.com>
2023-08-27 19:34:45 +0300

    Add butane calculator

--- test/component/test_nitrogen_calculator.py
+++ test/component/test_nitrogen_calculator.py
@@ -30,18 +30,18 @@ class NitrogenCalculatorTest(unittest.TestCase):
         total_grams = 100
         nitrogen_fraction = 0.9  # 90%
 
-        # N grams = total grams * nitrogen fraction = 100 * 0.9 = 90.0
+        # (NH₄)₂CO₃
         # N molar mass = 14.007
         # H molar mass = 1.008
         # C molar mass = 12.011
         # O molar mass = 15.999
-        # Ammonium molar mass = 14.007 * 2 + 1.008 * 8 + 12.011 + 15.999 * 3 = 28.014 + 8.064 + 12.011 + 47.997 = 96.086
-        # N molecules = N grams / Ammonium molar mass * Avogadro
-        # N molecules = 90 / 96.086 * 6.02e+23 = 0.9366609079366401 * 6.02e+23 = 5.638698665778574e+23
+        # N grams = total grams * nitrogen fraction = 100 * 0.9 = 90.0
+        # molar mass = 14.007 * 2 + 1.008 * 8 + 12.011 + 15.999 * 3 = 28.014 + 8.064 + 12.011 + 47.997 = 96.086
+        # moles = mass in grams / molar mass = 90 / 96.086 = 0.9366609079366401
+        # molecules = moles * Avogadro = 90 / 96.086 * 6.02e+23 = 0.9366609079366401 * 6.02e+23 = 5.638698665778574e+23
 
-        # N mole = N grams / Ammonium molar mass = 90 / 96.086 = 0.9366609079366401
-        # N 2 mole = 0.9366609079366401 + 0.9366609079366401 = 1.8733218158732803
-        # N atoms = N 2 mole * Avogadro = 1.8733218158732803 * 6.02e+23 = 1.1277397331557148e+24
+        # 2 moles = 0.9366609079366401 + 0.9366609079366401 = 1.8733218158732803
+        # N atoms = 2 moles * Avogadro = 1.8733218158732803 * 6.02e+23 = 1.1277397331557148e+24
 
         # when
         result = nitrogen_calculator.calculate_ammonium_compound_atoms(total_grams, nitrogen_fraction)
@@ -69,18 +69,17 @@ class NitrogenCalculatorTest(unittest.TestCase):
         total_grams = 20
         nitrogen_fraction = 0.9  # 90%
 
-        # (NH₂)2CO grams = total grams * nitrogen fraction = 20 * 0.9 = 18
+        # (NH₂)₂CO grams = total grams * nitrogen fraction = 20 * 0.9 = 18
         # N molar mass = 14.007
         # H molar mass = 1.008
         # C molar mass = 12.011
         # O molar mass = 15.999
-        # Urea molar mass = 14.007 * 2 + 1.008 * 4 + 12.011 + 15.999 = 28.014 + 4.032 + 12.011 + 15.999 = 60.056
-        # N molecules = N grams / Urea molar mass * Avogadro
-        # N molecules = 18 / 60.056 * 6.02e+23 = 0.29972026108964966 * 6.02e+23 = 1.804315971759691e+23
+        # molar mass = 14.007 * 2 + 1.008 * 4 + 12.011 + 15.999 = 28.014 + 4.032 + 12.011 + 15.999 = 60.056
+        # moles = mass in grams / molar mass = 18 / 60.056 = 0.29972026108964966
+        # molecules = moles * Avogadro = 0.29972026108964966 * 6.02e+23 = 1.804315971759691e+23
 
-        # N mole = N grams / Urea molar mass = 18 / 60.056 = 0.29972026108964966
-        # N 2 mole = 0.29972026108964966 + 0.29972026108964966 = 0.5994405221792993
-        # N atoms = N 2 mole * Avogadro = 0.5994405221792993 * 6.02e+23 = 3.608631943519382e+23
+        # 2 moles = 0.29972026108964966 + 0.29972026108964966 = 0.5994405221792993
+        # N atoms = 2 moles * Avogadro = 0.5994405221792993 * 6.02e+23 = 3.608631943519382e+23
 
         # when
         result = nitrogen_calculator.calculate_urea_compound_atoms(total_grams, nitrogen_fraction)
